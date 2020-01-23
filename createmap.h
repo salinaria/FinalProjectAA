bd8651608bc97b47d1e10453b3bafd12fb069e7b
aliaa80 <a.ansari80@outlook.com>
2020-01-23 15:43:42 +0330

    delete includes to using in main

--- createmap.h
+++ createmap.h
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int create_map(int n){
+int create_map(int size_map){
 	int i,j;
 	char k;
 	srand(time(NULL));
 	FILE *fp=fopen("map.bin","wb");
-	fwrite(&n,sizeof(int),1,fp);
-	for(i=0;i<n;i++){
-		for(j=n-1;j>=0;j--){
+	fwrite(&size,sizeof(int),1,fp);
+	for(i=0;i<size;i++){
+		for(j=size-1;j>=0;j--){
 			k='1'+rand()%4;
 			fwrite(&k,sizeof(char),1,fp);
 		}
